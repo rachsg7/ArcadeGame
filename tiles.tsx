72f0b863aa045236245e6b684faedf7201268f8b
rachsg7 <sch19051@byui.edu>
2022-11-26 10:14:17 -0800

    Game

---
+++ tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tiles" tilewidth="128" tileheight="128" tilecount="156" columns="13">
+ <image source="Lib/site-packages/arcade/resources/images/spritesheets/tiles.png" width="1664" height="1536"/>
+</tileset>
